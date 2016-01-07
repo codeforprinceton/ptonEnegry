e40d8ec79eb0b99a5a85320d19fd5f2b7a72508c
jackking11111111 <jackniuniunancy@gmail.com>
2016-01-07 09:33:06 -0500

    Revert "switched html"
    This reverts commit 4844d94e7b4001c181ec7afe777e5257b1c10e72.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,19 +1,19 @@
 class ApiController < ApplicationController
   def index
   end
-
+  
   def raw
       @alldata = Reading.all
       return render :json => @alldata.to_json(:except => [:created_at, :updated_at, :id])
   end
-
+  
   def rawtemp
       @alldata = Temperature.all.only(:date, :temp)
       return render :json => @alldata.to_json(:except => [:created_at, :updated_at, :id])
   end
-
+  
   def geojson
-      @data = File.read("#{Rails.root}/Stat model in 3D/Stat_Model.html").html_safe
+      @data = File.read("#{Rails.root}/public/princeton.geojson.json").html_safe
       return render :json => @data
   end
   
